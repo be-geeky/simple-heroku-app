57ce28f931ab5d9fe887262f6f5308c751eec7cd
Dushyant Joshi <dushyantjoshia@gmail.com>
2017-10-12 16:31:05 +0530

    Update test.php

--- webhook/test.php
+++ webhook/test.php
@@ -10,8 +10,8 @@ function processMessage($update) {
     $input = $test->format('H:i');
     $is_between_dates = isBetween($from, $to, $input);
 
-    //if ($update["result"]["action"] == "is_livechat" && $is_between_dates) {
-        $text = 'Please click <a href="https://www.teamhgs.com/we-want-hear-you"> here </a> for Business Related Inquiries';
+    if ($update["result"]["action"] == "is_livechat" && !$is_between_dates) {
+        $text = 'Please click <a href="https://www.teamhgs.com/we-want-hear-you" target="_blank"> here </a> for Business Related Inquiries';
         sendMessage(array(
             "source" => $update["result"]["source"],
             "speech" => $text,
@@ -19,7 +19,7 @@ function processMessage($update) {
             "contextOut" => array()
         ));
     }
-//}
+}
 
 function sendMessage($parameters) {
     echo json_encode($parameters);
