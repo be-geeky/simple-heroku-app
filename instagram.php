532d12a4a04ddb308de23390c8fe2bf00c9e1123
Dushyant Joshi <dushyant.joshi@alogic.co>
2020-01-23 16:50:07 +1030

    test

--- instagram.php
+++ instagram.php
@@ -3,3 +3,6 @@
 $username = 'dushyant.joshi.a';
 $json = file_get_contents('https://www.instagram.com/' . $username . '/media/');
 $instagram_feed_data = json_decode($json, true);
+
+echo "<pre>";
+print_r($instagram_feed_data);
